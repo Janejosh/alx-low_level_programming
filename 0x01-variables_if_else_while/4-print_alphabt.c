fc160f2e78ee7253a5f5aa79663691cbdb77a874
Janejosh <bukunmi9758@gmail.com>
2023-06-17 07:39:31 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+	if
+		(b != 'q' && b != 'e') {
+			putchar(b);
+		}
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
