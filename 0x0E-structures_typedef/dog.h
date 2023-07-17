d7ce9033400555aef6947d52810895991ef1df2d
Janejosh <bukunmi9758@gmail.com>
2023-07-17 11:02:45 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A program that define a dog's information
+ * @name: The name of dog
+ * @age: The age of dog
+ * @owner: The dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
